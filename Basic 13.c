7b7ee9fe85dd220e194cd956e747d002a4fa5539
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-11-27 23:48:58 +0530

    Update Basic 13.c

--- Basic 13.c
+++ Basic 13.c
@@ -5,17 +5,22 @@ int main() {
     int i, vowels = 0, consonants = 0;
 
     printf("Enter a string: ");
-    gets(str);   // simple for exams
+    gets(str);  
+    
+    // simple for exams
 
     for(i = 0; str[i] != '\0'; i++) {
         char ch = str[i];
 
         if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            
             // Check if vowel
             if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
                 ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U') {
                 vowels++;
-            } else {
+            } 
+            
+            else {
                 consonants++;
             }
         }
