ee43e2ffabee23735f2912d0e8d1106a1d357851
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-11-30 18:05:52 +0530

    Create Basic 27.c

---
+++ Basic 27.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Handle 0 separately
+    if (n == 0) {
+        count = 1;
+    } else {
+        // Convert negative number to positive
+        if (n < 0) {
+            n = -n;
+        }
+
+        // Count digits
+        while (n > 0) {
+            n = n / 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits = %d\n", count);
+
+    return 0;
+}
