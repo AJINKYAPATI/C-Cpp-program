d91d017a6dab6f89ef67379aaec0aea51a32ef1d
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-01 22:04:12 +0530

    Update Basic 1.c

--- Basic 1.c
+++ Basic 1.c
@@ -4,12 +4,14 @@ int main() {
     int n, i;
     long long fact = 1;
 
+    
     printf("Enter a number: ");
     scanf("%d", &n);
 
     if (n < 0) {
         printf("Factorial of a negative number is not possible.");
-    } else {
+    } 
+    else {
         for (i = 1; i <= n; i++) {
             fact *= i;
         }
