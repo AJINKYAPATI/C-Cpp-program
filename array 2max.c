209e07779612825f3104023fb42ab9bffc16e779
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-17 22:32:09 +0530

    Update array 2max.c

--- array 2max.c
+++ array 2max.c
@@ -4,18 +4,21 @@ int main(){
     int n,i;
     printf("enter a size for array:");
     scanf("%d",&n);
+   
     int arr[n];
 
     printf("enter value for array:");
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+    
     int max1,max2;
     
     if(arr[0] > arr[1]) {
         max1 = arr[0];
         max2 = arr[1];
-    }else {
+    }
+    else {
         max1 = arr[1];
         max2 = arr[0];
     }
@@ -24,7 +27,9 @@ int main(){
         if(arr[i] > max1) {
             max2 = max1;
             max1 = arr[i];
-        }else if(arr[i] > max2 && arr[i] != max1){
+        }
+        
+        else if(arr[i] > max2 && arr[i] != max1){
             max2 = arr[i];
         }
     }
