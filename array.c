d944fbe15785eb664ec323704b1c129b873e95e3
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-25 21:55:58 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-int main(){
+int main()
+{
     int arr[]={10,21,30,40,50,67,89,67};
     int len=sizeof(arr)/sizeof(arr[0]);
+    
     for(int i=0;i<len;i++){
         printf("%d\n",arr[i]);
     }
