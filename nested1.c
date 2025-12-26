dc751f0364e455ab1d3a4f44819000fa82c6328a
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-26 22:40:18 +0530

    Update nested1.c

--- nested1.c
+++ nested1.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-int main(){
+int main()
+{
 
     int a=1,n,b=1;
     printf("enter a number:");
     scanf("%d",&n);
 
-    while(a<=10){
+    while(a<=10)
+    {
         b=1;
         while(b<=n){
             printf("%d\t",a*b);
