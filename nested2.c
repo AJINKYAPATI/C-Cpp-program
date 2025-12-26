ac01909412421b3a6d5b7c5ede68664e9311a0e0
AJINKYAPATI <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-26 22:40:42 +0530

    Update nested2.c

--- nested2.c
+++ nested2.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
-int main(){
+int main()
+{
 
     int num;
     printf("enter one num");
     scanf("%d",&num);
 
-    if(num>=0){
+    if(num>=0)
+    {
         if(num%2==0){
         printf("num is even");
         }else{
