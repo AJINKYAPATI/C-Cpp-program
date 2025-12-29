3c8961646bf102312ca5783c5931b2ecf465b551
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-29 21:30:53 +0530

    Update 2D array.c

--- 2D array.c
+++ 2D array.c
@@ -10,11 +10,13 @@ int main()
     int arr2[row][col];
 
     printf("enter value for 2d array: ");
+    
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             scanf("%d",&arr2[i][j]);
         }
     }
+    
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             printf("%d ",arr2[i][j]);
