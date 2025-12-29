a37cf45a345035576de11dfec8d9e8198ca42be8
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2025-12-29 21:28:56 +0530

    Create radiosort.c

---
+++ radiosort.c
@@ -0,0 +1,27 @@
+
+int getMax(int arr[], int len)
+{
+int max=arr[0];
+for(int i = 0 i<len;i++){
+if(arr[i]>max) {
+max=arr[1];
+}
+}
+}
+return max;
+}
+I
+void radixsort(int arr[], int len){
+int max-getMax(arr, len);
+for(int i = 1 max/i>0;1*=10)
+countingSort(arr, len, 1);
+
+
+
+
+int main () {
+    int arr[]={33,45,567,78,98};
+    int len=sizeof(arr)/sizeof(arr[0]);
+    radixsort(arr,len);
+    return 0;
+}
