2ab211a4d57fc637ea3ed10fab0df9dfa610f9a3
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-29 22:08:39 +0530

    Update string 1.c

--- string 1.c
+++ string 1.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main()
-
 {
-
     char str[]={'h','e','l','l','o','\0'};
     char str[]="hello world";
 
     printf("%s\n",str);
     printf("%S\n",str1);
     printf("%C\n",str[3]);
-
-
-    return 0;
+return 0;
 }
